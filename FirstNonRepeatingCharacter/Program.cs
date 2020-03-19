c6aab1240ef1077304933c108ebc7d865c5c1205
Khud0 <pavement.cafe.37@gmail.com>
2020-03-19 01:35:41 +0100

    Solution Created

---
+++ FirstNonRepeatingCharacter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstNonRepeatingCharacter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Version No Git");
+        }
+    }
+}
