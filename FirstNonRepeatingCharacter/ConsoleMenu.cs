1ab5c3b7f80ed148e74cfb1ec014b2dc12ae6de0
Khud0 <pavement.cafe.37@gmail.com>
2020-03-19 13:10:41 +0100

    Code refactoring and documentation

---
+++ FirstNonRepeatingCharacter/ConsoleMenu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace CharacterSearch
+{
+    class ConsoleMenu
+    {
+        private static Stopwatch stopwatch = default;
+        // Correct answer: '1' - FirstNonrepeatingCharacter
+        // Correct answer: '1', 'e', 'p' - AllNonRepeatingCharacters
+        private static string testString = "aabbccdddgggggwwwwxzxzklqklqttttttyyyyyy1uiuiuiuiooooeffqqqvvvbnbnbnmmkkpkqtydf";
+        private static bool caseSensitiveSearch = true;
+
+        public static void Main(string[] args)
+        {
+            if (!caseSensitiveSearch) testString = testString.ToLower();
+
+            RunSearcher<FirstNonRepeatingCharacter>();
+
+            Console.ReadKey();
+        }
+
+        
+        private static void RunSearcher<T>() where T : CharacterSearcher, new()
+        {
+            StartStowpatch(ref stopwatch);
+            T searcher = new T();
+            searcher.Find(testString);
+            StopStopwatch(ref stopwatch);
+        }
+        private static void StartStowpatch(ref Stopwatch activeStopwatch)
+        {
+            if (activeStopwatch == default) 
+            {
+                activeStopwatch = Stopwatch.StartNew();
+            } else activeStopwatch.Restart();
+        }
+        private static void StopStopwatch(ref Stopwatch activeStopwatch)
+        {
+            if (activeStopwatch == default) { Console.WriteLine("Stopwatch hadn't been started before you tried to stop it"); return; }
+            
+            activeStopwatch.Stop();
+            Console.WriteLine("Elapsed time: {0}.", activeStopwatch.Elapsed);                      
+        }
+    }
+}
--- FirstNonRepeatingCharacter/FirstNonRepeatingCharacter.cs
+++ FirstNonRepeatingCharacter/FirstNonRepeatingCharacter.cs
@@ -2,16 +2,40 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace FirstNonRepeatingCharacter
+namespace CharacterSearch
 {
-    class PointerSearch : ISearchNonRepeatingCharacter
+    class FirstNonRepeatingCharacter : CharacterSearcher
     {
-        public override string ToString()
+        public override void Find(string stringToSearchIn)
         {
-            return "PointerSearch";
+            DisplaySearchResult("Two Pointers", TwoPointers(stringToSearchIn));
         }
 
-        public char FindFirstNonRepeatingCharacter(string stringToSearchIn)
+
+
+        protected override void DisplaySearchResult(string methodName, char foundCharacter)
+        {
+            if (foundCharacter == default)
+            {
+                Console.WriteLine("Method \"{0}\" didn't find any Non Repeating Charaters.", methodName);
+            } 
+            else 
+            {
+                Console.WriteLine("Method \"{0}\" found First Non Repeating Charater: {1}", methodName, foundCharacter);
+            }
+            
+        }
+
+
+
+        /// <summary>
+        /// The slowest FirstNonRepeatingCharacter method. It uses two loops:
+        /// <para>First loop goes through each character.</para>
+        /// <para>Second loop compares char from the first one to each other character in the string.</para>
+        /// </summary>
+        /// <param name="stringToSearchIn">Sir</param>
+        /// <returns>First non repeating character in the given string.</returns>
+        private char TwoPointers(string stringToSearchIn)
         {
             char foundCharacter = default;
             int stringLength = stringToSearchIn.Length;
@@ -39,9 +63,7 @@ namespace FirstNonRepeatingCharacter
                 }
             }
 
-            CharacterSearcher.DisplaySearchResult(this.ToString(), foundCharacter);
-
             return foundCharacter;
-        }
+        }   
     }
 }
